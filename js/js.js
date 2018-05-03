21b9caf6fc275d71f42ccf49ca7a211a5cc1d3b5
Jonny <jorfn@hotmail.co.uk>
2018-05-03 11:52:08 +0100

    SmoothScroll added, unused logos removed and main logo optimised

--- js/js.js
+++ js/js.js
@@ -11,3 +11,17 @@ function closeNav() {
     document.getElementById("menuButton").removeAttribute("class");
     document.getElementById("menuButton").setAttribute("class", "column large-hidden small-1");
 }
+$('a[href*=#]:not([href=#])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+      || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+         if (target.length) {
+           $('html,body').animate({
+               scrollTop: target.offset().top
+          }, 1000);
+          return false;
+      }
+  }
+});
