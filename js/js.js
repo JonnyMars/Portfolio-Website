d0bffc6d4c0cacf8f1c529f414f33ec9fd98cdab
Jonny <jorfn@hotmail.co.uk>
2018-05-04 09:16:03 +0100

    removed unused script

--- js/js.js
+++ js/js.js
@@ -1,13 +1,13 @@
 function openNav() {
     document.getElementById("myOffnav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    //document.getElementById("home").style.marginLeft = "250px";
     document.getElementById("menuButton").removeAttribute("class");
     document.getElementById("menuButton").setAttribute("class", "column small-hidden");
 }
 
 function closeNav() {
     document.getElementById("myOffnav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    //document.getElementById("home").style.marginLeft = "0px";
     document.getElementById("menuButton").removeAttribute("class");
     document.getElementById("menuButton").setAttribute("class", "column large-hidden small-1");
 }
